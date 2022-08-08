143a4e4908c01c46072bf4fab1cf01ae09b6929b
Daniela Martinez <rene2.martinez@outlook.com>
2022-08-08 00:26:17 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import WeatherApp from './components/WeatherApp'
+
+function App() {
+
+  const [coords, setCoords] = useState()
+  const [bgImg, setBgImg] = useState()
+
+  useEffect(() => {
+
+    const success = (pos) => {
+      const location = {
+        lat: pos.coords.latitude,
+        lon: pos.coords.longitude
+      }
+      setCoords(location)
+    }
+
+    navigator.geolocation.getCurrentPosition(success)
+  }, [bgImg])
+
+  console.log(coords);
+  
+
+  return (
+    <div className="App" style={bgImg}>
+      <WeatherApp lon={coords?.lon} lat={coords?.lat} setBgImg={setBgImg}/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/WeatherApp.jsx
@@ -0,0 +1,174 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+const WeatherApp = ({ lon, lat, setBgImg }) => {
+
+  const [weather, setWeather] = useState()
+  const [temperature, setTemperature] = useState()
+  const [isCeslsius, setIsCeslsius] = useState(true)
+
+  useEffect(() => {
+    if (lat) {
+      const APIKey = "9e4cc85e3d6a90e73ab6528b3d590405"
+      const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKey}`
+
+      axios.get(URL)
+        .then(res => {
+          setWeather(res.data)
+          const temp = {
+            celsius: {
+              celsius_temp: `${Math.round(res.data.main.temp - 273.15)}°C`,
+              celsius_min: `${Math.round(res.data.main.temp_min - 273.15)}°C`,
+              celsius_max: `${Math.round(res.data.main.temp - 273.15)}°C`,
+              celsius_feels_like: `${Math.round(res.data.main.feels_like - 273.15)}°C`
+            },
+            farenheit: {
+              farenheit_temp: `${Math.round((res.data.main.temp - 273.15) * 9 / 5 + 32)}°F`,
+              farenheit_min: `${Math.round((res.data.main.temp_min - 273.15) * 9 / 5 + 32)}°F`,
+              farenheit_max: `${Math.round((res.data.main.temp_max - 273.15) * 9 / 5 + 32)}°F`,
+              farenheit_feels_like: `${Math.round((res.data.main.feels_like - 273.15) * 9 / 5 + 32)}°F`
+            }
+          }
+          setTemperature(temp)
+
+
+          if (weather?.weather[0].description === "clear sky") {
+            let backgroundImg = {
+              backgroundImage: "url(../public/clear-sky.jpg)"
+            }
+            setBgImg(backgroundImg)
+          } else if (weather?.weather[0].main === "Thunderstorm") {
+            let backgroundImg = {
+              backgroundImage: "url(../public/thunderstorm.jpg)"
+            }
+            setBgImg(backgroundImg)
+          } else if (weather?.weather[0].main === "Drizzle") {
+            let backgroundImg = {
+              backgroundImage: "url(../public/Drizzle.jpg)"
+            }
+            setBgImg(backgroundImg)
+          } else if (weather?.weather[0].description === "scattered clouds") {
+            let backgroundImg = {
+              backgroundImage: "url(../public/scattered-clouds.jpg)"
+            }
+            setBgImg(backgroundImg)
+          } else if (weather?.weather[0].description === "few clouds") {
+            let backgroundImg = {
+              backgroundImage: "url(../public/few-clouds.jpg)"
+            }
+            setBgImg(backgroundImg)
+          } else if (weather?.weather[0].description === "broken clouds") {
+            let backgroundImg = {
+              backgroundImage: "url(../public/broken-clouds.jpg)"
+            }
+            setBgImg(backgroundImg)
+          } else if (weather?.weather[0].description === "overcast clouds") {
+            let backgroundImg = {
+              backgroundImage: "url(../public/overcast-clouds.jpg)"
+            }
+            setBgImg(backgroundImg)
+          } else if (weather?.weather[0].main === "Rain") {
+            let backgroundImg = {
+              backgroundImage: "url(../public/rain.jpg)"
+            }
+            setBgImg(backgroundImg)
+          } else if (weather?.weather[0].main === "Snow") {
+            let backgroundImg = {
+              backgroundImage: "url(../public/snow.jpg)"
+            }
+            setBgImg(backgroundImg)
+          } else if (weather?.weather[0].main === "Mist") {
+            let backgroundImg = {
+              backgroundImage: "url(../public/mist.jpg)"
+            }
+            setBgImg(backgroundImg)
+          } else if (weather?.weather[0].main === "Mist") {
+            let backgroundImg = {
+              backgroundImage: "url(../public/fog.jpg)"
+            }
+            setBgImg(backgroundImg)
+          } else if (weather?.weather[0].main === "Smoke") {
+            let backgroundImg = {
+              backgroundImage: "url(../public/smoke.jpg)"
+            }
+            setBgImg(backgroundImg)
+          } else if (weather?.weather[0].main === "Haze") {
+            let backgroundImg = {
+              backgroundImage: "url(../public/haze.jpg)"
+            }
+            setBgImg(backgroundImg)
+          } else if (weather?.weather[0].main === "Dust") {
+            let backgroundImg = {
+              backgroundImage: "url(../public/dust.jpg)"
+            }
+            setBgImg(backgroundImg)
+          } else if (weather?.weather[0].main === "Fog") {
+            let backgroundImg = {
+              backgroundImage: "url(../public/fog.jpg)"
+            }
+            setBgImg(backgroundImg)
+          } else if (weather?.weather[0].main === "Sand") {
+            let backgroundImg = {
+              backgroundImage: "url(../public/sand.jpg)"
+            }
+            setBgImg(backgroundImg)
+          } else if (weather?.weather[0].main === "Dust") {
+            let backgroundImg = {
+              backgroundImage: "url(../public/dust.jpg)"
+            }
+            setBgImg(backgroundImg)
+          } else if (weather?.weather[0].main === "Ash") {
+            let backgroundImg = {
+              backgroundImage: "url(../public/volcanic-ash.jpg)"
+            }
+            setBgImg(backgroundImg)
+          } else if (weather?.weather[0].main === "Squall") {
+            let backgroundImg = {
+              backgroundImage: "url(../public/squall.jpg)"
+            }
+            setBgImg(backgroundImg)
+          } else if (weather?.weather[0].main === "Tornado") {
+            let backgroundImg = {
+              backgroundImage: "url(../public/tornado.jpg)"
+            }
+            setBgImg(backgroundImg)
+          }
+
+        })
+        .catch(err => console.log(err))
+    }
+  }, [lat, lon])
+
+
+  console.log(weather);
+
+  const changeTemperature = () => setIsCeslsius(!isCeslsius)
+
+
+  return (
+    <nav className='weather'>
+
+      <div className='container-city'>
+        <h1 className='city'>{weather?.name}, {weather?.sys.country}</h1>
+        <img src={`http://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`} alt="" />
+        <p className='temperature'>{
+          isCeslsius ?
+            temperature?.celsius.celsius_temp :
+            temperature?.farenheit.farenheit_temp}
+        </p>
+      </div>
+      <ul>
+        <li>{weather?.weather[0].description}</li>
+        <li>Humidity: {weather?.main.humidity}%</li>
+      </ul>
+      <div className='container-temeperatures'>
+        <p>min:{isCeslsius ? temperature?.celsius.celsius_min : temperature?.farenheit.farenheit_min} </p>
+        <p>max: {isCeslsius ? temperature?.celsius.celsius_max : temperature?.farenheit.farenheit_max}</p>
+      </div>
+
+
+    </nav>
+  )
+}
+
+export default WeatherApp
